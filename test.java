ec37ee1f93d2acb1922195355e45b603e23f5d9d
onesirian <ldlockhart@gmail.com>
2019-11-28 20:11:35 -0600

    11.28.19 commit

---
+++ test.java
@@ -0,0 +1,9 @@
+public class test{
+    public static void main(String[] args) {
+        for (int i=0;i<10;i++) {
+            for (int j=0; j<5; j++) {
+                System.out.println(""+i+j);
+            }
+        }
+    }
+}

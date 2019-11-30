0e379bb686f089d165ea47aa9185b22b5b2ff72b
onesirian <ldlockhart@gmail.com>
2019-11-30 11:08:00 -0600

    finish word count mini-program

--- filescan/WordCount/src/WordCount.java
+++ filescan/WordCount/src/WordCount.java
@@ -3,15 +3,18 @@ import java.io.File;
 
 public class WordCount {
 
-    public static void main (String [] args) throws Exception {
+    public static void main(String [] args) throws Exception{
 
         File file = new File("mobydick.txt");
         Scanner scannedFile = new Scanner(file);
+
+        int words = 0;
         while (scannedFile.hasNextLine()) {
             String currentLine = scannedFile.nextLine();
-            System.out.println(currentLine);
+            words += currentLine.split("").length;
         }
-     }
+        System.out.println("Your file has a total of "+words+" words.");
+    }
 
 
 }

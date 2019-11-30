255136e5b2b3e00325270a7410e591e48b377ca0
onesirian <ldlockhart@gmail.com>
2019-11-30 15:55:16 -0600

    add skeleton WheelofFortune game

---
+++ WheelOfFortune/src/WheelOfFortune.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.io.File;
+
+public class WheelOfFortune {
+    public static void main(String [] args) {
+        System.out.println("Welcome to Wheel of Fortune!");
+        File file = new File("movies.txt");
+        Scanner gameFile = new Scanner(file);
+
+
+    }
+}
+

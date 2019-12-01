48d5d8d8f41c531fc9ef331a62ecdf42ed1af031
onesirian <ldlockhart@gmail.com>
2019-11-30 18:01:30 -0600

    choose random line

--- WheelOfFortune/src/WheelOfFortune.java
+++ WheelOfFortune/src/WheelOfFortune.java
@@ -1,13 +1,23 @@
 import java.util.Scanner;
 import java.io.File;
+import java.lang.Math;
 
 public class WheelOfFortune {
-    public static void main(String [] args) {
+    public static void main(String [] args) throws Exception {
         System.out.println("Welcome to Wheel of Fortune!");
+
         File file = new File("movies.txt");
         Scanner gameFile = new Scanner(file);
+        int lineNumber = (int)(Math.random()*25);
+        int i = 0;
+        while (i<lineNumber) {
+            gameFile.nextLine();
+            i++;
+        }
 
-
+        System.out.println(lineNumber);
+        String currentLine = gameFile.nextLine();
+        System.out.println(currentLine);
     }
 }
 

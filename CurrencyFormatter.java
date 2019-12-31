4d55cfa929a9b87fb54b09f084f9c8e701fb8af5
onesirian <ldlockhart@gmail.com>
2019-12-30 20:50:53 -0600

    Add initial main method and print statments

---
+++ CurrencyFormatter.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+public class CurrencyFormatter {
+	public static void main() {
+
+		 Scanner scanner = new Scanner(System.in);
+		 double payment = scanner.nextDouble();
+                 scanner.close();
+
+		 System.out.println("US: " + us);
+		 System.out.println("India: " + india);
+	         System.out.println("China: " + china);
+		 System.out.println("France: " + france);
+		
+
+	}
+
+}
+
+

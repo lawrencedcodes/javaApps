e7fe9046822e581c0c205d0a25188ad4f6c07fce
onesirian <ldlockhart@gmail.com>
2019-12-30 22:38:33 -0600

    almost home!

--- CurrencyFormatter.java
+++ CurrencyFormatter.java
@@ -6,16 +6,21 @@ import java.util.regex.*;
 
 
 public class CurrencyFormatter {
-	public static void main() {
+	public static void main(String[] args) {
 
 		 Scanner scanner = new Scanner(System.in);
 		 double payment = scanner.nextDouble();
                  scanner.close();
+                 
+                 String usC = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+                 String inC = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+		 String cnC = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+		 String fnC = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
 
-		 System.out.println("US: " + us);
-		 System.out.println("India: " + india);
-	         System.out.println("China: " + china);
-		 System.out.println("France: " + france);
+		 System.out.println("US: "+usC);
+		 System.out.println("India: "+inC);
+	         System.out.println("China: "+cnC);
+		 System.out.println("France: "+fnC);
 		
 
 	}

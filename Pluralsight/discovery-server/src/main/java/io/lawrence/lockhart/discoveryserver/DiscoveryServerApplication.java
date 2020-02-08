a85eb01f3ca770bb398473dd059f1571de4e6add
Lawrence Lockhart <ldlockhart@gmail.com>
2020-02-08 13:51:56 -0600

    Minor config changes

--- Pluralsight/discovery-server/src/main/java/io/lawrence/lockhart/discoveryserver/DiscoveryServerApplication.java
+++ Pluralsight/discovery-server/src/main/java/io/lawrence/lockhart/discoveryserver/DiscoveryServerApplication.java
@@ -2,11 +2,14 @@ package io.lawrence.lockhart.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello World");
 		SpringApplication.run(DiscoveryServerApplication.class, args);
 	}
 

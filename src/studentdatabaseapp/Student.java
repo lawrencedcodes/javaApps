71f1e12e6a32a0636a302dbf9390cf6827dbc9b7
Lawrence D. Lockhart, Jr <ldlockhart@gmail.com>
2020-06-06 17:44:55 -0500

    Start student database app

---
+++ src/studentdatabaseapp/Student.java
@@ -0,0 +1,18 @@
+package studentdatabaseapp;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private Integer studentId;
+    //Constructor prompt for student name and year
+
+    //Generate an ID
+
+    //Enroll in courses
+
+    //View balance, pay tuition
+
+    //Show status
+
+}

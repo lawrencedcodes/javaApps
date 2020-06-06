f9e54be3c874e1e2148946634fe20143b1c81fd3
Lawrence D. Lockhart, Jr <ldlockhart@gmail.com>
2020-06-06 18:24:58 -0500

    setup constructor to take in student data

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -1,12 +1,27 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class Student {
     private String firstName;
     private String lastName;
-    private String gradeYear;
+    private Integer gradeYear;
     private Integer studentId;
+    private String courses;
+    private Integer tuitionBalance;
+    private Integer costOfCourse = 600;
     //Constructor prompt for student name and year
+    public Student() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter student first name ");
+        this.firstName = in.nextLine();
+        System.out.println("Enter student last name ");
+        this.lastName = in.nextLine();
+        System.out.println("1-Freshman\n 2-Sophmore\n 3-Junior\n 4-Senior\n Enter student class level ");
+        this.gradeYear = in.nextInt();
 
+        System.out.println(firstName+ " "+lastName+ " " +gradeYear);
+    }
     //Generate an ID
 
     //Enroll in courses

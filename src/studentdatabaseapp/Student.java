5a22d310d597881b9078c2777d3b59eddc39eaf9
Lawrence D. Lockhart, Jr <ldlockhart@gmail.com>
2020-06-07 06:33:08 -0500

    Add increment for studentID

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -6,23 +6,34 @@ public class Student {
     private String firstName;
     private String lastName;
     private Integer gradeYear;
-    private Integer studentId;
+    private String studentId;
     private String courses;
     private Integer tuitionBalance;
-    private Integer costOfCourse = 600;
+    private static Integer costOfCourse = 600;
+    private static Integer id = 1001;
+
     //Constructor prompt for student name and year
     public Student() {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter student first name ");
         this.firstName = in.nextLine();
+
         System.out.println("Enter student last name ");
         this.lastName = in.nextLine();
+
         System.out.println("1-Freshman\n 2-Sophmore\n 3-Junior\n 4-Senior\n Enter student class level ");
         this.gradeYear = in.nextInt();
+        setStudentId();
+        System.out.println(firstName+ " "+lastName+ " " +gradeYear+ " "+studentId);
 
-        System.out.println(firstName+ " "+lastName+ " " +gradeYear);
+
     }
     //Generate an ID
+    private void setStudentId() {
+        //Grade Level + ID
+        this.studentId = gradeYear + "" + id;
+        id++;
+    }
 
     //Enroll in courses
 

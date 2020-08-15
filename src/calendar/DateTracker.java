b87a68b20cc5834e4f8fcf6b3c3d2659f5da4b26
Lawrence D. Lockhart, Jr <ldlockhart@gmail.com>
2020-08-15 01:03:17 -0500

    clean up code

---
+++ src/calendar/DateTracker.java
@@ -0,0 +1,12 @@
+package calendar;
+import java.time.LocalDate;
+import java.util.Calendar;
+
+public class DateTracker {
+    public static void main(String[] args) {
+        java.util.Calendar rightnow = java.util.Calendar.getInstance();
+        rightnow.setWeekDate(2015, 8, 5);
+        System.out.println((rightnow.getTime()));
+        }
+}
+

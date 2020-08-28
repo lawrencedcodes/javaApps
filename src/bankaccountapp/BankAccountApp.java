1233b45b7818cc87f52f90bda24f35847ce2b08b
Lawrence D. Lockhart, Jr <ldlockhart@gmail.com>
2020-08-28 05:28:50 -0500

    Add csv reader function

--- src/bankaccountapp/BankAccountApp.java
+++ src/bankaccountapp/BankAccountApp.java
@@ -1,18 +1,28 @@
 package bankaccountapp;
 
+import java.util.List;
+
 public class BankAccountApp {
 
     public static void main(String[] args) {
+        String file = "C:\\Users\\ldloc\\Documents\\Coding\\javaApps\\src\\bankaccountapp\\NewBankAccounts.csv";
         //Read CSV file
+        List<String[]> newAccountHolders = utilities.CSV.read(file);
+        for (String[] accountHolder:newAccountHolders) {
+            System.out.println("New Account");
+            System.out.println(accountHolder[0]);
+            System.out.println(accountHolder[1]);
+            System.out.println(accountHolder[2]);
+            System.out.println(accountHolder[3]);
+        }
+
         //Create new account based on CSV data
-        Checking chkacct1 = new Checking("John Doe", "123456789",5000);
-        Savings svacct1 = new Savings("Bota Rwanda", "987654321", 2000);
-
-        svacct1.compound();
-
-        svacct1.showInfo();
-        System.out.println("**************");
-        chkacct1.showInfo();
+//        Checking chkacct1 = new Checking("John Doe", "123456789",5000);
+//        Savings svacct1 = new Savings("Bota Rwanda", "987654321", 2000);
+//        svacct1.compound();
+//        svacct1.showInfo();
+//        System.out.println("**************");
+//        chkacct1.showInfo();
 
     }
 }

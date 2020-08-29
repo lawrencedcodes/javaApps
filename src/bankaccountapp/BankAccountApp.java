6710df7c212f638def438d11642a9ee8361bcda3
Lawrence D. Lockhart, Jr <ldlockhart@gmail.com>
2020-08-29 08:46:53 -0500

    Access account object, iterate, print

--- src/bankaccountapp/BankAccountApp.java
+++ src/bankaccountapp/BankAccountApp.java
@@ -1,11 +1,14 @@
 package bankaccountapp;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class BankAccountApp {
 
     public static void main(String[] args) {
         String file = "C:\\Users\\ldloc\\Documents\\Coding\\javaApps\\src\\bankaccountapp\\NewBankAccounts.csv";
+        List<Account> accounts = new LinkedList<Account>();
+
         //Read CSV file
         List<String[]> newAccountHolders = utilities.CSV.read(file);
         for (String[] accountHolder:newAccountHolders) {
@@ -14,16 +17,22 @@ public class BankAccountApp {
             String ssN = accountHolder[1];
             String accountType = accountHolder[2];
             double initDeposit = Double.parseDouble(accountHolder[3]);
-            System.out.println("Name: "+name+"ssn: "+ssN+"Account Type: "+accountType+" Initial Deposit: "+initDeposit);
+           // System.out.println("Name: "+name+"ssn: "+ssN+"Account Type: "+accountType+" Initial Deposit: "+initDeposit);
+            if (accountType.equals("Savings")) {
+                accounts.add(new Savings(name,ssN,initDeposit));
+            } else if (accountType.equals("Checking")){
+                accounts.add(new Checking(name,ssN,initDeposit));
+            } else {
+                System.out.println("Error reading account type, please try again.");
+            }
         }
-
-        //Create new account based on CSV data
-//        Checking chkacct1 = new Checking("John Doe", "123456789",5000);
-//        Savings svacct1 = new Savings("Bota Rwanda", "987654321", 2000);
-//        svacct1.compound();
-//        svacct1.showInfo();
-//        System.out.println("**************");
-//        chkacct1.showInfo();
-
+        for (Account acc:accounts) {
+            System.out.println("\n*********");
+            acc.showInfo();
+        }
+        // todo Perform random transactions
+        // todo i.e. accounts.get((int) Math.random() * accounts.size()).deposit(1000);
+        // todo iterate through accounts, print balance, etc.
+        // todo native gui, web gui, mobile gui
     }
 }

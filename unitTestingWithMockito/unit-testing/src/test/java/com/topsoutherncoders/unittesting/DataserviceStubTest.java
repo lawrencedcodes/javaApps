5e00f670b0c2c675a914362897bfd9ece418934e
3807558 <lawrence.lockhart@fedex.com>
2020-09-07 11:43:21 -0500

    Add 2 stubs

--- unitTestingWithMockito/unit-testing/src/test/java/com/topsoutherncoders/unittesting/DataserviceStubTest.java
+++ unitTestingWithMockito/unit-testing/src/test/java/com/topsoutherncoders/unittesting/DataserviceStubTest.java
@@ -8,13 +8,25 @@ import javax.xml.crypto.Data;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DataserviceStub implements Dataservice {
-
     @Override
     public int[] retrieveAllData() {
         return new int[]{1,2,3};
     }
 }
 
+class DataserviceEmptyStub implements Dataservice {
+    @Override
+    public int[] retrieveAllData() {
+        return new int[]{};
+    }
+}
+
+class DataserviceRepeatingStub implements Dataservice {
+    @Override
+    public int[] retrieveAllData() {
+        return new int[]{9,9,9,9,9,9};
+    }
+}
 class DataserviceStubTest {
 
     @Test
@@ -28,6 +40,7 @@ class DataserviceStubTest {
     @Test
     void calcSumEmptyWithService() {
         BusinessImpl business = new BusinessImpl();
+        business.setDataservice(new DataserviceEmptyStub());
         int testResult = business.calcSumWithService(); //new int[] {}
         int expected = 0;
         assertEquals(expected, testResult);
@@ -36,9 +49,9 @@ class DataserviceStubTest {
     @Test
     void calcSumRepeatingWithService() {
         BusinessImpl business = new BusinessImpl();
-        int[] repeatingArray = {9,9,9,9,9,9,9,9};
+        business.setDataservice(new DataserviceRepeatingStub());
         int testResult = business.calcSumWithService(); //repeatingArray
-        int expected = repeatingArray[0] * repeatingArray.length;
+        int expected = 54;
         assertEquals(expected, testResult);
     }
 }
